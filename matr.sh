b9de19e4d2cbe94049c00b08e3a0675dae662e6c
JanDimple <jandimple17@gmail.com>
2025-10-02 18:23:31 +0200

    Update cr.sh

--- matr.sh
+++ matr.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 rm -rf .repo/local_manifests/
-repo init -u https://github.com/ProjectMatrixx/android.git -b 15.0 --git-lfs
-git clone https://github.com/armmv7a/local_manifests.git -b matr .repo/local_manifests
+repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
+git clone https://github.com/JanDimple/local_manifests.git -b earth .repo/local_manifests
 /opt/crave/resync.sh
-export BUILD_USERNAME=armv7a
+export BUILD_USERNAME=JanDimple
 export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export KBUILD_USERNAME=armv7a
+export KBUILD_USERNAME=JanDimple
 export KBUILD_HOSTNAME=crave
-export TZ=Asia/Kuching
+export TZ=Europe/Warsaw
 . build/envsetup.sh
 brunch earth
