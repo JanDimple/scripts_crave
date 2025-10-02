3492ae98787625cb04e1ca49fca2a4e470113bbe
JanDimple <jandimple17@gmail.com>
2025-10-02 18:26:07 +0200

    Update and rename matr.sh to cr.sh

--- cr.sh
+++ cr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf .repo/local_manifests/
 repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
-git clone https://github.com/JanDimple/local_manifests.git -b earth .repo/local_manifests
+git clone https://github.com/JanDimple/local_manifests.git -b gale .repo/local_manifests
 /opt/crave/resync.sh
 export BUILD_USERNAME=JanDimple
 export BUILD_HOSTNAME=crave
@@ -10,4 +10,4 @@ export KBUILD_USERNAME=JanDimple
 export KBUILD_HOSTNAME=crave
 export TZ=Europe/Warsaw
 . build/envsetup.sh
-brunch earth
+brunch gale
